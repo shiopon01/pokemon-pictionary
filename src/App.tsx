ac96eb3564891bd37de986f5d358d9bef945e74c
shiopon01 <shio.0323@gmail.com>
2022-11-24 02:05:31 +0900

    mod App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/pokemon/images/001.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          ポケモンの名前当てゲームができる予定
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" />
+        <input type="button" value="送信" />
       </header>
     </div>
   );
